882a7590b20f87457733c36237fd4242b666be02
Rishabh20041 <138551209+Rishabh20041@users.noreply.github.com>
2023-07-07 17:51:00 +0530

    Add files via upload

---
+++ Assignment_2-2.py
@@ -0,0 +1,32 @@
+#Identity Operators:
+
+#is
+
+a1 = [1, 2, 3]
+b1 = a1
+c1 = [1, 2, 3]
+print(a1 is b1)  
+print(a1 is c1)  
+
+#isNot
+
+a1 = [1, 2, 3]
+b1 = a1
+c1 = [1, 2, 3]
+print(a1 is not b1)  
+print(a1 is not c1)  
+
+#Membership Operators:
+
+#in
+
+colour = ['RED', 'BLUE', 'GREEN']
+print('BLUE' in colour)    
+print('BLACK' in colour)     
+
+#not in
+
+colour = ['RED', 'BLUE', 'GREEN']
+
+print('BLUE' not in colour) 
+print('BLACK' not in colour)  
